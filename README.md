545337d07774ac777928f315476f4ad976de2847
Tsiry Sandratraina <tsiry.sndr@gmail.com>
2023-09-04 18:49:31 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,63 @@
+# Prisma Pipeline
+
+[![fluentci pipeline](https://img.shields.io/badge/dynamic/json?label=pkg.fluentci.io&labelColor=%23000&color=%23460cf1&url=https%3A%2F%2Fapi.fluentci.io%2Fv1%2Fpipeline%2Fprisma_pipeline&query=%24.version)](https://pkg.fluentci.io/prisma_pipeline)
+![deno compatibility](https://shield.deno.dev/deno/^1.34)
+[![](https://img.shields.io/codecov/c/gh/fluent-ci-templates/prisma-pipeline)](https://codecov.io/gh/fluent-ci-templates/prisma-pipeline)
+
+A ready-to-use CI/CD Pipeline for managing your database migrations with [Prisma Migrate](https://www.prisma.io/docs/guides/migrate)
+
+## 🚀 Usage
+
+Run the following command in your project:
+
+```bash
+dagger run fluentci prisma_pipeline
+```
+
+Or, if you want to use it as a template:
+
+```bash
+fluentci init -t prisma
+```
+
+This will create a `.fluentci` folder in your project.
+
+Now you can run the pipeline with:
+
+```bash
+dagger run fluentci .
+```
+
+## Environment variables
+
+| Variable         | Description                    |
+| ---------------- | ------------------------------ |
+| DATABASE_URL     | The database connection string |
+
+## Jobs
+
+| Job       | Description               |
+| --------- | ------------------------- |
+| validate  | Validate prisma schema    |
+| deploy    | Deploy all migrations     |
+| push      | Apply schema changes      |
+
+## Programmatic usage
+
+You can also use this pipeline programmatically:
+
+```ts
+import { Client, connect } from "https://esm.sh/@dagger.io/dagger@0.8.1";
+import { Dagger } from "https://pkg.fluentci.io/prisma_pipeline/mod.ts";
+
+const { deploy, validate } = Dagger;
+
+function pipeline(src = ".") {
+  connect(async (client: Client) => {
+    await validate(client, src);
+    await deploy(client, src);
+  });
+}
+
+pipeline();
+```
---
+++ src/dagger/jobs.ts
@@ -0,0 +1,134 @@
+import Client from "@dagger.io/dagger";
+
+export enum Job {
+  validate = "validate",
+  deploy = "deploy",
+  push = "push",
+}
+
+const DATABASE_URL = Deno.env.get("DATABASE_URL");
+
+const exclude = [".git", "node_modules", ".fluentci", ".env"];
+
+export const validate = async (client: Client, src = ".") => {
+  const context = client.host().directory(src);
+  const ctr = client
+    .pipeline(Job.push)
+    .container()
+    .from("ghcr.io/fluent-ci-templates/bun:latest")
+    .withMountedCache(
+      "/app/node_modules",
+      client.cacheVolume("prisma_node_modules")
+    )
+    .withDirectory("/app", context, { exclude })
+    .withWorkdir("/app")
+    .withExec(["sh", "-c", 'eval "$(devbox global shellenv)" && bun install'])
+    .withExec([
+      "sh",
+      "-c",
+      `eval "$(devbox global shellenv)" && bun x prisma validate`,
+    ]);
+
+  const result = await ctr.stdout();
+
+  console.log(result);
+};
+
+export const deploy = async (client: Client, src = ".") => {
+  if (!DATABASE_URL) {
+    throw new Error("DATABASE_URL is not set");
+  }
+
+  const mysql = client
+    .container()
+    .from("mysql")
+    .withEnvVariable("MYSQL_ROOT_PASSWORD", "pass")
+    .withEnvVariable("MYSQL_DATABASE", "example")
+    .withExposedPort(3306);
+
+  const context = client.host().directory(src);
+  const ctr = client
+    .pipeline(Job.push)
+    .container()
+    .from("ghcr.io/fluent-ci-templates/bun:latest")
+    .withServiceBinding("mysql", mysql)
+    .withMountedCache(
+      "/app/node_modules",
+      client.cacheVolume("prisma_node_modules")
+    )
+    .withDirectory("/app", context, { exclude })
+    .withWorkdir("/app")
+    .withEnvVariable("DATABASE_URL", DATABASE_URL)
+    .withExec(["sh", "-c", 'eval "$(devbox global shellenv)" && bun install'])
+    .withExec([
+      "sh",
+      "-c",
+      `eval "$(devbox global shellenv)" && bun x prisma migrate deploy`,
+    ]);
+
+  const result = await ctr.stdout();
+
+  console.log(result);
+};
+
+export const push = async (client: Client, src = ".") => {
+  if (!DATABASE_URL) {
+    throw new Error("DATABASE_URL is not set");
+  }
+
+  const mysql = client
+    .container()
+    .from("mysql")
+    .withEnvVariable("MYSQL_ROOT_PASSWORD", "pass")
+    .withEnvVariable("MYSQL_DATABASE", "example")
+    .withExposedPort(3306);
+
+  const context = client.host().directory(src);
+  const ctr = client
+    .pipeline(Job.push)
+    .container()
+    .from("ghcr.io/fluent-ci-templates/bun:latest")
+    .withServiceBinding("mysql", mysql)
+    .withMountedCache(
+      "/app/node_modules",
+      client.cacheVolume("prisma_node_modules")
+    )
+    .withDirectory("/app", context, { exclude })
+    .withWorkdir("/app")
+    .withEnvVariable("DATABASE_URL", DATABASE_URL)
+    .withExec(["sh", "-c", 'eval "$(devbox global shellenv)" && bun install'])
+    .withExec([
+      "sh",
+      "-c",
+      `eval "$(devbox global shellenv)" && bun x prisma db push`,
+    ]);
+
+  const result = await ctr.stdout();
+
+  console.log(result);
+};
+
+export type JobExec = (
+  client: Client,
+  src?: string
+) =>
+  | Promise<void>
+  | ((
+      client: Client,
+      src?: string,
+      options?: {
+        ignore: string[];
+      }
+    ) => Promise<void>);
+
+export const runnableJobs: Record<Job, JobExec> = {
+  [Job.validate]: validate,
+  [Job.deploy]: deploy,
+  [Job.push]: push,
+};
+
+export const jobDescriptions: Record<Job, string> = {
+  [Job.validate]: "Validate prisma schema",
+  [Job.deploy]: "Deploy all migrations",
+  [Job.push]: "Apply schema changes",
+};
